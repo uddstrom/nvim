a7b6f0d69aeb97fa8d14806e14c80891e82aad1a
Jesper <jesper@uddstrom.se>
2023-02-20 23:54:12 +0100

    add lsp zero

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,25 @@
+local lsp = require('lsp-zero').preset({
+  name = 'minimal',
+  set_lsp_keymaps = true,
+  manage_nvim_cmp = true,
+  suggest_lsp_servers = true,
+})
+
+lsp.ensure_installed({
+    'tsserver',
+    'eslint',
+})
+
+-- (Optional) Configure lua language server for neovim
+lsp.nvim_workspace()
+
+lsp.setup()
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
