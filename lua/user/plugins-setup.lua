181a7b0671275593b0aead4edcef8551baf39586
Jesper <jesper@uddstrom.se>
2023-02-25 00:01:27 +0100

    add null-ls for formatting and linting

--- lua/user/plugins-setup.lua
+++ lua/user/plugins-setup.lua
@@ -26,21 +26,18 @@ end
 
 return packer.startup(function(use)
     use("wbthomason/packer.nvim")
-    
+    -- lua functions that many plugins use
+    use("nvim-lua/plenary.nvim")
     -- color scheme
     use("bluz71/vim-nightfly-guicolors")
-    
     -- fun games to learn vim
-    use("ThePrimeagen/vim-be-good") 
-    
+    use("ThePrimeagen/vim-be-good")
     -- file explorer
     use("nvim-tree/nvim-tree.lua")
     -- icons
     use("kyazdani42/nvim-web-devicons")
-    
     -- syntax highlighting
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-
     -- status line
     use("nvim-lualine/lualine.nvim")
 
@@ -66,6 +63,10 @@ return packer.startup(function(use)
         {'rafamadriz/friendly-snippets'}, -- Optional
       }
     }
+
+    -- formatting and linting
+    use("jose-elias-alvarez/null-ls.nvim")
+    use("jayp0521/mason-null-ls.nvim")
 
     if packer_bootstrap then
         require("packer").sync()
