2e9049f1506bf7a5afd99a3d3fc7bb5701418d0d
Jesper <jesper@uddstrom.se>
2023-02-25 13:47:08 +0100

    add autopairs and gitsigns

--- lua/user/plugins-setup.lua
+++ lua/user/plugins-setup.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -21,55 +21,61 @@ vim.cmd([[
 
 local status, packer = pcall(require, "packer")
 if not status then
-    return
+	return
 end
 
 return packer.startup(function(use)
-    use("wbthomason/packer.nvim")
-    -- lua functions that many plugins use
-    use("nvim-lua/plenary.nvim")
-    -- color scheme
-    use("bluz71/vim-nightfly-guicolors")
-    -- fun games to learn vim
-    use("ThePrimeagen/vim-be-good")
-    -- file explorer
-    use("nvim-tree/nvim-tree.lua")
-    -- icons
-    use("kyazdani42/nvim-web-devicons")
-    -- syntax highlighting
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-    -- status line
-    use("nvim-lualine/lualine.nvim")
+	use("wbthomason/packer.nvim")
+	-- lua functions that many plugins use
+	use("nvim-lua/plenary.nvim")
+	-- color scheme
+	use("bluz71/vim-nightfly-guicolors")
+	-- fun games to learn vim
+	use("ThePrimeagen/vim-be-good")
+	-- file explorer
+	use("nvim-tree/nvim-tree.lua")
+	-- icons
+	use("kyazdani42/nvim-web-devicons")
+	-- syntax highlighting
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+	-- status line
+	use("nvim-lualine/lualine.nvim")
 
-    use {
-      'VonHeikemen/lsp-zero.nvim',
-      branch = 'v1.x',
-      requires = {
-        -- LSP Support
-        {'neovim/nvim-lspconfig'},             -- Required
-        {'williamboman/mason.nvim'},           -- Optional
-        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v1.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{ "williamboman/mason.nvim" }, -- Optional
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},         -- Required
-        {'hrsh7th/cmp-nvim-lsp'},     -- Required
-        {'hrsh7th/cmp-buffer'},       -- Optional
-        {'hrsh7th/cmp-path'},         -- Optional
-        {'saadparwaiz1/cmp_luasnip'}, -- Optional
-        {'hrsh7th/cmp-nvim-lua'},     -- Optional
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "hrsh7th/cmp-buffer" }, -- Optional
+			{ "hrsh7th/cmp-path" }, -- Optional
+			{ "saadparwaiz1/cmp_luasnip" }, -- Optional
+			{ "hrsh7th/cmp-nvim-lua" }, -- Optional
 
-        -- Snippets
-        {'L3MON4D3/LuaSnip'},             -- Required
-        {'rafamadriz/friendly-snippets'}, -- Optional
-      }
-    }
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" }, -- Required
+			{ "rafamadriz/friendly-snippets" }, -- Optional
+		},
+	})
 
-    -- formatting and linting
-    use("jose-elias-alvarez/null-ls.nvim")
-    use("jayp0521/mason-null-ls.nvim")
+	-- formatting and linting
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("jayp0521/mason-null-ls.nvim")
 
-    if packer_bootstrap then
-        require("packer").sync()
-    end
+	-- auto closing
+	use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
+	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+
+	-- git integration
+	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
+
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
-
